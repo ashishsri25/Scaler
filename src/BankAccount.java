aee51e63f5e818a030342083dfe1bb6b09c7fe1e
ashishsri25 <28553697+ashishsri25@users.noreply.github.com>
2026-04-13 12:57:59 +0530

    Initial commit

---
+++ src/BankAccount.java
@@ -0,0 +1,32 @@
+import java.awt.image.BandCombineOp;
+
+public class BankAccount {
+    double balance;
+    String ownerName;
+
+    public double deposit(double amount) {
+        balance += amount;
+        return balance;
+    }
+
+    public double withdraw(double amount) {
+        if(balance ==0 || balance < amount) {
+//            double v = -1.0;
+            return -1.0;
+        }
+        return balance -=amount;
+    }
+
+    public static void main(String[] args) {
+        BankAccount bk = new BankAccount();
+        System.out.println("Updated balance after deposit is " + bk.deposit(1000));
+        double withAmount = 2500.00;
+        double res= bk.withdraw(withAmount);
+        if(res==-1){
+            System.out.println("Your Current balance is :" + bk.balance +". Account balance is low to withdraw amount: "+ withAmount + ". Please try with lesser amount" );
+        }
+        else{
+            System.out.println("Updated balance after withdraw is "+ bk.balance);
+        }
+    }
+}
---
+++ src/BankAccount2.java
@@ -0,0 +1,40 @@
+public class BankAccount2 {
+    private double balance;
+    String ownerName;
+
+    public double deposit(double amount) {
+        balance += amount;
+        return balance;
+    }
+
+    public double withdraw(double amount) {
+        if(balance ==0 || balance < amount) {
+//            double v = -1.0;
+            return -1.0;
+        }
+        return balance -=amount;
+    }
+
+    public void getBalance() {
+        System.out.println("Your current balance is " + balance);
+    }
+
+    public void setBalance(double amount) {
+        balance += amount;
+    }
+
+    public static void main(String[] args) {
+        BankAccount2 bk = new BankAccount2();
+        bk.setBalance(2000);
+        bk.getBalance();
+        double withdrawAmount = 1500;
+        System.out.println("Withdraw amount is " + withdrawAmount);
+        double res= bk.withdraw(withdrawAmount);
+        if(res==-1){
+            System.out.println("Your Current balance is :" + bk.balance +". Account balance is low to withdraw amount: "+ withdrawAmount + ". Please try with lesser amount" );
+        }
+        else{
+            bk.getBalance();
+        }
+    }
+}
